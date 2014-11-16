9910bc837ce6cfa8b193209a0b1d59f4d2e5afc9
unknown <dell@.(none)>
2014-11-16 15:45:29 +0530

    password_digest error

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -3,6 +3,7 @@ class Hotel < ActiveRecord::Base
   has_many :users
   has_many :categories
   has_many :offers
+  has_secure_password
    mount_uploader :hotelImage, ImageUploader
   scope :sorted, lambda{order("hotels.hotel_location ASC")}
 end
