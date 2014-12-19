7db3b29d88e86486cdd0e9a3c067fb484f8c23e2
unknown <dell@.(none)>
2014-12-19 14:53:52 +0530

    alter file removed

--- db/migrate/20140721151923_alter_users.rb
+++
@@ -1,9 +0,0 @@
-class AlterUsers < ActiveRecord::Migration
-  def up          
-    rename_column("users","password","hashed_password")   
-  end
-  
-  def down
-   rename_column("users","hashed_password","password")  
-  end
-end
--- db/migrate/20140827061010_alter_menus.rb
+++
@@ -1,9 +0,0 @@
-class AlterMenus < ActiveRecord::Migration
-  def up
-    
-    add_column("menus","category_id",:integer,:after=>"item_type")
-    add_index("menus","category_id")
-     add_column("menus","veg",:integer,:after=>"category_id")
-      add_column("menus","non_veg",:integer,:after=>"veg")
-  end
-end
